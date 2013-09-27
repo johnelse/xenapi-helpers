99a583a4c53e59418c10aa57c1bb6bd8d1706dc8
John Else <john.else@citrix.com>
2013-09-27 12:34:22 +0100

    Reindent main.py
    Signed-off-by: John Else <john.else@citrix.com>

--- main.py
+++ main.py
@@ -6,31 +6,31 @@ session = None
 api = None
 
 def get_API_session(host, username, password):
-  session = XenAPI.Session("http://%s" % host)
-  try:
-    session.xenapi.login_with_password(username, password)
-  except Exception, e:
-    msg = "Could not login to API session. Error: %s" % str(e)
-    print msg
-    raise Exception(msg)
-  return session
+    session = XenAPI.Session("http://%s" % host)
+    try:
+        session.xenapi.login_with_password(username, password)
+    except Exception, e:
+        msg = "Could not login to API session. Error: %s" % str(e)
+        print msg
+        raise Exception(msg)
+    return session
 
 def connect(host, username="root", password="xenroot"):
-  global cached_host, session, api
-  cached_host = host
-  session = get_API_session(host, username, password)
-  api = session.xenapi
-  print "You now have the following objects to play with:"
-  print "session - an API session object for %s." % host
-  print "api - an API dispatcher for %s (just an alias for session.xenapi)." % host
+      global cached_host, session, api
+      cached_host = host
+      session = get_API_session(host, username, password)
+      api = session.xenapi
+      print "You now have the following objects to play with:"
+      print "session - an API session object for %s." % host
+      print "api - an API dispatcher for %s (just an alias for session.xenapi)." % host
 
 def usage():
-  print "Usage: %s <hostname> [<username> [<password>]]" % sys.argv[0]
+    print "Usage: %s <hostname> [<username> [<password>]]" % sys.argv[0]
 
 if __name__ == "__main__":
-  print ""
-  if len(sys.argv) < 2:
-    usage()
-  else:
-    args = sys.argv[1:]
-    connect(*args)
+    print ""
+    if len(sys.argv) < 2:
+        usage()
+    else:
+        args = sys.argv[1:]
+        connect(*args)
